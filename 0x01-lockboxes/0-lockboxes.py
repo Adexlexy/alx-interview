1fa4096f76c7eb16147c4cb971852621d9844d7e
Adexlexy <oshewolo_martins@yahoo.com>
2023-08-03 16:56:15 +0100

    Error fixed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,42 +1,33 @@
 #!/usr/bin/python3
-"""A python module that determines if all boxes can be opened
-   from a list of lists
-"""
+""" Lockboxes interview coding challenge """
+
+from typing import List
 
 
-def canUnlockAll(boxes=[]):
-    """A function that returns True of all box in
-    boxes can be opend
+def canUnlockAll(boxes: List[list]) -> bool:
+    """ checks if the all boxes can be unlocked
+
+    Args:
+        boxes (List[list]): List of boxes
+
+    Returns:
+        bool: True if all boxes can be unlocked else fals
     """
-    if not boxes:
+    unlocked_boxes = {}
+    try:
+        empty_index = boxes.index([])
+        boxes[empty_index] = [0]
+    except Exception:
+        pass
+    box_numbers = [x for x in range(len(boxes))]
+    # print(some)
+    unlocked_boxes[0] = 'unlocked'
+    for i in range(len(boxes) - 1):
+        for k in range(len(boxes[i])):
+            if boxes[i][k] in box_numbers:
+                unlocked_boxes[boxes[i][k]] = 'unlocked'
+
+    opened = unlocked_boxes.values()
+    if len(opened) != len(boxes):
         return False
-
-    keys = set([0])
-    for box_id, box in enumerate(boxes):
-        for key in box:
-            if key < len(boxes) and key != box_id:
-                keys.add(key)
-
-    if len(keys) != len(boxes):
-        return False
-
     return True
-
-
-if __name__ == '__main__':
-    boxes = [
-                [1, 3],
-                [2],
-                [3, 0],
-                [1, 2, 3],
-            ]
-    print(unlockBoxes(boxes))
-
-    boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))
-
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(unlockBoxes(boxes))
-
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(unlockBoxes(boxes))
